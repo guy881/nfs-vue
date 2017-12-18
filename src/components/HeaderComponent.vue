e64ed1c6f867395ddf5081cfc3f9026e408159b1
Paweł Mucha <pawel.mucha@yahoo.pl>
2017-12-18 13:56:49 +0100

    Add navbar

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,30 +1,32 @@
 <template>
   <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <router-link :to="{ name: 'home' }" class="navbar-brand">NFS</router-link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <div class="container">
+        <router-link :to="{ name: 'home' }" class="navbar-brand">NFS</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <router-link :to="{ name: 'home' }" class="nav-link" exact="">Scanning</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link :to="{ name: 'scans' }" class="nav-link" exact="">Previous scans</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link :to="{ name: 'hardware' }" class="nav-link" exact="">Hardware Configuration</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link :to="{ name: 'import' }" class="nav-link" exact="">Import</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link :to="{ name: 'settings' }" class="nav-link" exact="">Settings</router-link>
-          </li>
-        </ul>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <router-link :to="{ name: 'home' }" class="nav-link" exact="">Scanning</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{ name: 'scans' }" class="nav-link" exact="">Previous scans</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{ name: 'hardware' }" class="nav-link" exact="">Hardware Configuration</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{ name: 'import' }" class="nav-link" exact="">Import</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{ name: 'settings' }" class="nav-link" exact="">Settings</router-link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   </header>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
-
+<div class="container">
+<h1>Hello pretty boy!</h1>
+  <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur aliquet quam id dui posuere blandit. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Sed porttitor lectus nibh. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Curabitur aliquet quam id dui posuere blandit. Pellentesque in ipsum id orci porta dapibus. Pellentesque in ipsum id orci porta dapibus.</p>
+</div>
 </template>
 
 <script>
