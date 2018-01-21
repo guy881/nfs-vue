6ce31eac66272f0be08f825b3501b741c56b07b3
Paweł Mucha <pawel.mucha@yahoo.pl>
2018-01-21 01:14:22 +0100

    Visualizations finally from DB

--- src/views/ScanningResult.vue
+++ src/views/ScanningResult.vue
@@ -21,6 +21,7 @@
 <script>
   /* eslint-disable no-undef */
   import {HTTP} from '../http'
+  import {array, arange} from 'numjs'
 
   export default {
     name: 'ScanningResult',
@@ -29,7 +30,16 @@
         scan: {
           name: '',
           status: null,
-          progress: 0
+          progress: 0,
+          result: null
+        },
+        result: {
+          x: [],
+          y: [],
+          z: [],
+          f: [],
+          e: [],
+          reduced: []
         },
         errors: null
       }
@@ -39,7 +49,10 @@
       this.getScan()
     },
     mounted: function () { // DOM ready
-      this.createVisualization()
+      window.arange = arange
+      window.array = array
+      console.log(array)
+      console.log(arange(16).reshape(2, 2, 2, 2))
     },
     methods: {
       getScan: function () {
@@ -49,10 +62,45 @@
             console.log(response.data)
             if (!this.scan.pk) {
               this.scan = response.data
-            } else {
-              this.scan.status = response.data.status
-              this.scan.progress = response.data.progress
+              if (this.scan.result) {
+                this.displayResults()
+              }
             }
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      },
+      reduce4Dto2D: function (a) {
+        return a.map((x) => {
+          return x.map((y) => {
+            return y.reduce((z1, z2) => {
+              return z2.reduce((f3, f4) => { return f3 + f4 }, z1)
+            }, 0)
+          })
+        })
+      },
+      displayResults: function () {
+        const url = 'http://127.0.0.1:8000/results/' + this.scan.result
+        HTTP.get(url)
+          .then(response => {
+            this.result.x = JSON.parse(response.data.x)
+            this.result.y = JSON.parse(response.data.y)
+            this.result.z = JSON.parse(response.data.z)
+            this.result.f = JSON.parse(response.data.f)
+            this.result.e = JSON.parse(response.data.e)
+            // this.result.e = array(JSON.parse(response.data.e))
+            // console.log(this.result.e.shape)
+            // console.log(this.result.e.reduce)
+            console.log(this.result.e)
+            this.result.reduced = this.reduce4Dto2D(this.result.e)
+            console.log(this.result.reduced)
+            window.reduced = this.result.reduced
+            // const g = arange(81).reshape(3, 3, 3, 3).tolist()
+            // console.log(g)
+            // const p = this.reduce4Dto2D(g)
+            // console.log(p)
+            this.createVisualization()
           })
           .catch(error => {
             console.log(error)
@@ -60,37 +108,9 @@
       },
       createVisualization: function () {
         const data = [{
-          x: [-0.02, -0.015, -0.01, -0.005, 0, 0.005, 0.01, 0.015, 0.02],
-          y: [-0.02, -0.015, -0.01, -0.005, 0, 0.005, 0.01, 0.015, 0.02],
-          z: [
-            [11.27318318, 147.28138899, 365.90469269, 747.47607279,
-              3055.38326749, 1064.70509474, 918.79888497, 822.52352193,
-              831.75579348],
-            [141.34969225, 15.07407478, 239.51464804, 583.54133923,
-              1554.34179601, 1336.96208156, 1408.08111142, 1189.08922684,
-              1013.52085742],
-            [357.34668711, 224.62034929, 27.22973268, 489.50380689,
-              1877.85873517, 2274.15433703, 2072.51558244, 1586.78597368,
-              1252.26582965],
-            [741.85491557, 574.29708358, 494.26136916, 78.66529394,
-              1989.34262141, 10055.43138663, 2649.27779398, 1756.43455652,
-              1362.43842076],
-            [2640.65254686, 1858.92841057, 1949.26652161, 1862.52500545,
-              29.80298632, 3388.20922497, 2109.34259072, 1502.56176314,
-              1254.94265648],
-            [1057.80364877, 1338.02011381, 2293.61323227, 6206.56340623,
-              2022.64096821, 90.31987549, 834.33223421, 928.31253227,
-              963.29774471],
-            [928.36934844, 1415.97442831, 2071.85048736, 2637.88249158,
-              2138.04481608, 836.99230554, 26.61125047, 387.96604103,
-              620.20939035],
-            [826.89030338, 1190.27421579, 1586.47513448, 1765.650341,
-              1494.54573223, 930.54145908, 395.34233907, 14.67344341,
-              290.23165843],
-            [829.99294836, 1015.64568043, 1252.02178137, 1359.61867121,
-              1251.65019378, 970.98948563, 628.9369939, 291.85988414,
-              11.68525064]
-          ],
+          x: this.result.x,
+          y: this.result.y,
+          z: this.result.reduced,
           colorscale: 'Viridis',
           type: 'surface'
         }]
